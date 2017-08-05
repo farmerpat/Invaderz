7195285a345d76af9dd766a50fbc69169f01905f
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2017-08-05 11:11:41 -0400

    init enemy fleet ctrlr

---
+++ Assets/Scripts/EnemyFleetController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFleetController : MonoBehaviour {
+	public int level = 1;
+	private int fleetRows;
+	private int maxShotsOnScreen;
+	// the fleet should be a 2d array
+	// of enemy prefabs
+
+	void Start () {
+		this.fleetRows = level;
+		this.maxShotsOnScreen = 4 * level;
+		this.GenerateFleet ();
+		this.SpawnFleet ();
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	private void GenerateFleet () {
+		// build the 2d FleetCollection array
+
+	}
+
+	private void SpawnFleet () {
+		// draw the 2d FleetCollection array to screen
+
+	}
+}
