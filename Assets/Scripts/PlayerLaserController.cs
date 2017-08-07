2da7cb40efcf18b6fbef83cea17fa75784840035
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2017-08-07 03:27:31 -0400

    init PlayerLaser

---
+++ Assets/Scripts/PlayerLaserController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLaserController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Shoot (Vector2 pos) {
+
+	}
+}
